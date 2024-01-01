661be42b4f87fee7665a81877c18ec9db27c5719
Aditya <adityraj6508@gmail.com>
2024-01-01 22:40:00 +0530

    added lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+local plugin = {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup({
+            options = {
+                theme = 'dracula',
+            }
+        })
+    end,
+}
+
+return plugin
