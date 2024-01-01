fc9b7e7aa0b0067a4b1414a2a0fe0018d27695fd
Aditya <adityraj6508@gmail.com>
2024-01-01 19:21:54 +0530

    added treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,23 @@
-local plugin = {}
+local plugin = {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {
+                "lua",
+                "vim",
+                "vimdoc",
+                "c",
+                "cpp",
+                "cuda",
+                "python",
+                "rust",
+            },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
 
 return plugin
