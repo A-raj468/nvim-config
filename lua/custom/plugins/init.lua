1e5dd4059a5cf77b42ac9c948d1930f2ebe48d9c
Aditya <adityaraj6508@gmail.com>
2024-03-13 02:32:59 +0530

    Added nvim config based on kickstart
    Added codium and vim-be-good as custom plugins

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+    -- codium
+    {
+        'Exafunction/codeium.vim',
+        event = 'BufEnter',
+    },
+    -- vim be good
+    {
+        'ThePrimeagen/vim-be-good',
+        lazy = false,
+    },
+}
