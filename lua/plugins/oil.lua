c817dbfdb41f23850755d48107c947d0586d990f
Aditya <adityaraj6508@gmail.com>
2024-08-10 21:36:17 +0530

    Added keymap for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,8 +1,16 @@
 return {
     {
         'stevearc/oil.nvim',
-        opts = {},
+        opts = {
+            skip_confirm_for_simple_edits = true,
+        },
         -- Optional dependencies
         dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+        config = function(opts)
+            require('oil').setup(opts)
+            vim.keymap.set('n', '<leader>of', function()
+                require('oil').open '.'
+            end, { desc = 'Open [O]il [F]ile Browser' })
+        end,
     },
 }
