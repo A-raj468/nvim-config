406baba8e96e73bc94a3b060eca2dd1fb84fb942
Aditya <adityaraj6508@gmail.com>
2025-05-12 16:54:37 +0530

    Added macro recording in status line, fixed noice

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,6 +8,11 @@ return {
         opts = {
             sections = {
                 lualine_x = {
+                    {
+                        require('noice').api.statusline.mode.get,
+                        cond = require('noice').api.statusline.mode.has,
+                        color = { fg = '#ff9e64' },
+                    },
                     function()
                         local ok, pomo = pcall(require, 'pomo')
                         if not ok then
