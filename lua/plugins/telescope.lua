bc291467e261720a3828c3578953a5b8bd805c30
Aditya <adityaraj6508@gmail.com>
2025-05-23 14:32:21 +0530

    Changed telescope keymaps to start with f instead of s

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -39,35 +39,22 @@ return {
             require('telescope').load_extension 'ui-select'
 
             local builtin = require 'telescope.builtin'
-            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-            vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch [G]lobally' })
-            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-            vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-            vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+            vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[Find] [K]eymaps' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+            vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[Find] [S]elect Telescope' })
+
+            vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[Find] [R]esume' })
+            vim.keymap.set('n', '<leader>ft', ':Telescope themes<CR>', { silent = true, desc = '[Find] [T]heme' })
+            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[Find] [D]iagnostics' })
+
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[Find] [F]iles' })
             vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-            vim.keymap.set('n', '<leader>st', ':Telescope themes<CR>', { silent = true, desc = '[S]earch [T]heme' })
 
             vim.keymap.set('n', '<leader>/', function()
-                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-                    winblend = 10,
-                    previewer = false,
-                })
-            end, { desc = '[/] Fuzzily search in current buffer' })
-
-            vim.keymap.set('n', '<leader>s/', function()
-                builtin.live_grep {
-                    grep_open_files = true,
-                    prompt_title = 'Live Grep in Open Files',
-                }
-            end, { desc = '[S]earch [/] in Open Files' })
-
-            vim.keymap.set('n', '<leader>sn', function()
-                builtin.find_files { cwd = vim.fn.stdpath 'config' }
-            end, { desc = '[S]earch [N]eovim files' })
+                builtin.current_buffer_fuzzy_find()
+            end, { desc = '[/] Fuzzily find in current buffer' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[Find] [G]lobally' })
+            vim.keymap.set('n', '<leader>fc', builtin.grep_string, { desc = '[Find] [C]urrent word' })
         end,
     },
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -5,16 +5,12 @@ return {
         event = 'VeryLazy',
         opts = {
             spec = {
-                { '<leader>c', group = '[C]ode' },
-                { '<leader>d', group = '[D]ocument' },
-                { '<leader>s', group = '[S]earch' },
+                { '<leader>f', group = '[F]ind' },
                 { '<leader>w', group = '[W]orkspace' },
                 { '<leader>t', group = '[T]oggle' },
                 {
                     mode = { 'n', 'v' },
-                    { '<leader>h', group = 'Git [H]unk' },
-                    { '<leader>r', group = '[R]efactor' },
-                    { '<leader>ri', group = '[R]efactor [I]nline' },
+                    { '<leader>h', group = '[H]arpoon' },
                 },
             },
         },
